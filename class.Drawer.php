fb1b65886c7af3fe780014213eb87a17df7eecdf
Stefan Wilhelm <swilhelm@bplusd.de>
2017-07-20 18:46:09 +0200

    draws a nice animal

--- class.Drawer.php
+++ class.Drawer.php
@@ -5,66 +5,114 @@ class Drawer{
 	protected $imageWidth;
 	protected $imageHeight;
 	protected $imageResource;
+	protected $colorCollection = array();
+
+	protected $gapfromLeft;
+	protected $gapfromRight;
+	protected $gapfromTop;
+	protected $gapfromBottom;
 
 	public function __construct($width, $height){
 		
-		$this->imageWidth 		= $width;
-		$this->imageHeight 		= $height;
-		$this->imageResource 	= @imagecreatetruecolor($width, $height);
+		$this->imageWidth 					= $width;
+		$this->imageHeight 					= $height;
+		$this->imageResource 				= @imagecreatetruecolor($width, $height);
+		$this->colorCollection['white'] 	= imagecolorallocate($this->imageResource,255,255,255);
+		$this->colorCollection['black'] 	= imagecolorallocate($this->imageResource,0,0,0);
 
-		// Allocate colors
-		$pink = imagecolorallocate($canvas, 255, 105, 180);
-		$white = imagecolorallocate($canvas, 255, 255, 255);
-		$green = imagecolorallocate($canvas, 132, 135, 28);
-
-		// Draw three rectangles each with its own color
-		imagerectangle($canvas, 50, 50, 150, 150, $pink);
-		imagerectangle($canvas, 45, 60, 120, 100, $white);
-		imagerectangle($canvas, 100, 120, 75, 160, $green);
-
-		// Output and free from memory
-		header('Content-Type: image/jpeg');
-
-		imagejpeg($canvas);
-		imagedestroy($canvas);
+		imagefill($this->imageResource, 0, 0, $this->colorCollection['white']);
 	}
 
-	public function drawFrame($color, $style="left"){
+	public function createColorPalette($typeColor="RGB", $colorName, $colorValue){
+		switch($typeColor){
+			case "RGB":
+			$colors = explode($colorValue);
+			break;
+
+			case "HEX":
+			$colors = $this->createRGBColor($colorValue);
+			break;
+		}
+
+		$this->colorCollection[$colorName] = imagecolorallocate($this->getImageResource(), $colors[0], $colors[1], $colors[2]);
+	}
+
+	public function renderImage(){
+		header('Content-Type: image/jpeg');
+		imagejpeg($this->imageResource);
+	}
+
+	public function destroy(){
+		imagedestroy($this->getImageResource());
+	}
+
+	public function drawGutter($color, $spaceX, $spaceY){
+		$colorArray = $this->getColorCollection();
+
+		$numVerticalLines = ($this->getImageWidth() - (($this->getGapfromLeft() + $this->getGapfromRight())+2)) / $spaceX;
+		$numHorizontalLines = ($this->getImageHeight() - (($this->getGapfromTop() + $this->getGapfromBottom())+2)) / $spaceY;
+		$PositionX = 0;
+		$PositionY = 0;
+
+		for($x=1; $x<=$numVerticalLines; $x++){
+
+			$PositionX = $x * $spaceX + 1;
+
+			imageline ( $this->getImageResource(), $PositionX + $this->getGapfromLeft(), 0 + $this->getGapfromBottom(), $PositionX + $this->getGapfromLeft(), $this->getImageHeight() - $this->getGapfromTop(), $colorArray[$color]);
+		}
+
+		for($y=1; $y<=$numHorizontalLines; $y++){
+
+			$PositionY = $y * $spaceY + 1;
+
+			imageline ( $this->getImageResource(), 0 + $this->getGapfromLeft(), $PositionY + $this->getGapfromBottom(), $this->getImageWidth() - $this->getGapfromRight(), $PositionY + $this->getGapfromBottom()  ,$colorArray[$color]);
+		}
+	}
+
+	public function drawFrame($color, $style="left", $gapfromLeft=0, $gapfromRight=0, $gapfromTop=0, $gapfromBottom=0){ 
+
+		$this->setGapfromLeft($gapfromLeft);
+		$this->setGapfromRight($gapfromRight);
+		$this->setGapfromTop($gapfromTop);
+		$this->setGapfromBottom($gapfromBottom);
+
+		$colorArray = $this->getColorCollection();
+
 		switch($style){
 			case "full":
 				//vertical 0
-				imageline ( $this->getImageResource(), 0, 0, 0, $this->getImageHeight(), $colorInt);
+				imageline ( $this->getImageResource(), 0 + $this->getGapfromLeft(), 0 + $this->getGapfromTop(), 0 + $this->getGapfromLeft(), $this->getImageHeight() - $this->getGapfromBottom(), $colorArray[$color]);
 				//vertical 100
-				imageline ( $this->getImageResource(), $this->getImageWidth(), 0, $this->getImageWidth(), $this->getImageHeight(), $colorInt);
+				imageline ( $this->getImageResource(), $this->getImageWidth() - $this->getGapfromRight(), 0 + $this->getGapfromTop(), $this->getImageWidth() - $this->getGapfromRight(), $this->getImageHeight() - $this->getGapfromBottom(), $colorArray[$color]);
 
 				//horizontal 0
-				imageline ( $this->getImageResource(), 0, 0, $this->getImageWidth(), 0, $colorInt);
+				imageline ( $this->getImageResource(), 0 + $this->getGapfromLeft() , 0 + $this->getGapfromTop(), $this->getImageWidth() - $this->getGapfromRight(), 0 + $this->getGapfromTop(), $colorArray[$color]);
 				//horizontal 100
-				imageline ( $this->getImageResource(), 0, $this->getImageHeight(), $this->getImageWidth(), $this->getImageHeight(), $colorInt);
+				imageline ( $this->getImageResource(), 0 + $this->getGapfromLeft(), $this->getImageHeight() - $this->getGapfromBottom(), $this->getImageWidth() - $this->getGapfromRight(), $this->getImageHeight() - $this->getGapfromBottom(), $colorArray[$color]);
 			break;
 
 			case "left":
 				//vertical 0
-				imageline ( $this->getImageResource(), 0, 0, 0, $this->getImageHeight(), $colorInt);
+				imageline ( $this->getImageResource(), 0 + $this->getGapfromLeft(), 0 + $this->getGapfromTop(), 0 + $this->getGapfromLeft(), $this->getImageHeight() - $this->getGapfromBottom(), $colorArray[$color]);
 				//horizontal 100
-				imageline ( $this->getImageResource(), 0, $this->getImageHeight(), $this->getImageWidth(), $this->getImageHeight(), $colorInt);
+				imageline ( $this->getImageResource(), 0 + $this->getGapfromLeft(), $this->getImageHeight() - $this->getGapfromBottom(), $this->getImageWidth() - $this->getGapfromRight(), $this->getImageHeight() - $this->getGapfromBottom(), $colorArray[$color]);
 			break;
 
 			case "right":
 				//vertical 100
-				imageline ( $this->getImageResource(), $this->getImageWidth(), 0, $this->getImageWidth(), $this->getImageHeight(), $colorInt);
+				imageline ( $this->getImageResource(), $this->getImageWidth() - $this->getGapfromRight(), 0 + $this->getGapfromTop(), $this->getImageWidth() - $this->getGapfromRight(), $this->getImageHeight() - $this->getGapfromBottom(), $colorArray[$color]);
 				//horizontal 100
-				imageline ( $this->getImageResource(), 0, $this->getImageHeight(), $this->getImageWidth(), $this->getImageHeight(), $colorInt);
+				imageline ( $this->getImageResource(), 0 + $this->getGapfromLeft(), $this->getImageHeight() - $this->getGapfromBottom(), $this->getImageWidth() - $this->getGapfromRight(), $this->getImageHeight() - $this->getGapfromBottom(), $colorArray[$color]);
 			break;
 
 			case "top":
 				//horizontal 0
-				imageline ( $this->getImageResource(), 0, 0, $this->getImageWidth(), 0, $colorInt);
+				imageline ( $this->getImageResource(), 0 + $this->getGapfromLeft() , 0 + $this->getGapfromTop(), $this->getImageWidth() - $this->getGapfromRight(), 0 + $this->getGapfromTop(), $colorArray[$color]);
 			break;
 
 			case "bottom":
 				//horizontal 100
-				imageline ( $this->getImageResource(), 0, $this->getImageHeight(), $this->getImageWidth(), $this->getImageHeight(), $colorInt);
+				imageline ( $this->getImageResource(), 0 + $this->getGapfromLeft(), $this->getImageHeight() - $this->getGapfromBottom(), $this->getImageWidth() - $this->getGapfromRight(), $this->getImageHeight() - $this->getGapfromBottom(), $colorArray[$color]);
 			break;
 		}
 	}
@@ -76,6 +124,23 @@ class Drawer{
 
 	public function createHexColor($rgb = array()){
 		return sprintf("#%02x%02x%02x", $rgb[0], $rgb[1], $rgb[2]);
+	}
+
+
+	public function drawCircle($centerX, $centerY, $sizeX, $sizeY, $color, $border=0, $borderColor, $transparent=0) {
+
+		$colorArray = $this->getColorCollection();
+
+		if($transparent==0){
+			if($border!=0){
+				imagefilledellipse($this->getImageResource(), $centerX, $centerY, $sizeX, $sizeY, $colorArray[$borderColor]);	
+			}
+			imagefilledellipse($this->getImageResource(), $centerX, $centerY, $sizeX - $border, $sizeY - $border, $colorArray[$color]);
+		} else{
+			for($x=0; $x<=$border; $x++){
+				imageellipse($this->getImageResource(), $centerX, $centerY, $sizeX -$x, $sizeY -$x, $colorArray[$borderColor]);
+			}
+		}
 	}
 
     /**
@@ -134,6 +199,126 @@ class Drawer{
     public function setImageResource($imageResource)
     {
         $this->imageResource = $imageResource;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getColorCollection()
+    {
+        return $this->colorCollection;
+    }
+
+    /**
+     * @param mixed $colorCollection
+     *
+     * @return self
+     */
+    public function setColorCollection($colorCollection)
+    {
+        $this->colorCollection[] = $colorCollection;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getImageWidthGap()
+    {
+        return $this->imageWidthGap;
+    }
+
+    /**
+     * @param mixed $imageWidthGap
+     *
+     * @return self
+     */
+    public function setImageWidthGap($imageWidthGap)
+    {
+        $this->imageWidthGap = $imageWidthGap;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getGapfromLeft()
+    {
+        return $this->gapfromLeft;
+    }
+
+    /**
+     * @param mixed $gapfromLeft
+     *
+     * @return self
+     */
+    public function setGapfromLeft($gapfromLeft)
+    {
+        $this->gapfromLeft = $gapfromLeft;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getGapfromRight()
+    {
+        return $this->gapfromRight;
+    }
+
+    /**
+     * @param mixed $gapfromRight
+     *
+     * @return self
+     */
+    public function setGapfromRight($gapfromRight)
+    {
+        $this->gapfromRight = $gapfromRight;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getGapfromTop()
+    {
+        return $this->gapfromTop;
+    }
+
+    /**
+     * @param mixed $gapfromTop
+     *
+     * @return self
+     */
+    public function setGapfromTop($gapfromTop)
+    {
+        $this->gapfromTop = $gapfromTop;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getGapfromBottom()
+    {
+        return $this->gapfromBottom;
+    }
+
+    /**
+     * @param mixed $gapfromBottom
+     *
+     * @return self
+     */
+    public function setGapfromBottom($gapfromBottom)
+    {
+        $this->gapfromBottom = $gapfromBottom;
 
         return $this;
     }
--- index.php
+++ index.php
@@ -1,8 +1,67 @@
 <?php 
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once('class.drawer.php');
 
-$painting = new Drawer();
+$painting = new Drawer(800, 600);
 
+//$painting->drawFrame('black', $style='full', 100, 100, 100, 100);
+
+$painting->createColorPalette('HEX','grey', '#CCCCCC');
+//$painting->drawGutter('grey', 25, 25);
+//$painting->drawCircle(400, 300, 100, 50, 'black', 10, 'green',0);
+
+//animal
+$painting->createColorPalette('HEX','blue', '#99eadf');
+$painting->createColorPalette('HEX','lightblue', '#c4fffb');
+$painting->createColorPalette('HEX','brown', '#59433b');
+$painting->createColorPalette('HEX','lightbrown', '#ab6d52');
+$painting->createColorPalette('HEX','darkgrey', '#a49e96');
+
+//background
+$color = $painting->getColorCollection();
+imagefill($painting->getImageResource(), 0, 0, $color['blue']);
+$painting->drawCircle(400, 400, 400, 80, 'lightblue', 0, '',0);
+
+//tail
+for ($i=0; $i <=200 ; $i++) { 
+	$painting->drawCircle(300 + $i, 330 + ($i*2), 80, 80, 'brown', 0, '',0);
+}
+//ears
+$painting->drawCircle(335, 240, 40, 40, 'brown', 0, '',0);
+$painting->drawCircle(465, 240, 40, 40, 'brown', 0, '',0);
+//body
+for ($i=0; $i <=200 ; $i++) { 
+	$painting->drawCircle(400, 300 + $i, 160, 160, 'lightbrown', 0, '',0);
+}
+//eyes
+$painting->drawCircle(370, 290, 20, 20, 'black', 0, '',0);
+$painting->drawCircle(430, 290, 20, 20, 'black', 0, '',0);
+//teeth
+$painting->drawCircle(385, 360, 20, 70, 'white', 0, '',0);
+$painting->drawCircle(415, 360, 20, 70, 'white', 0, '',0);
+imagefilledrectangle($painting->getImageResource() , 350 , 385 , 430 , 420 , $color['lightbrown'] );
+//beard
+$painting->drawCircle(380, 345, 55, 40, 'darkgrey', 0, '',0);
+$painting->drawCircle(420, 345, 55, 40, 'darkgrey', 0, '',0);
+//nose
+$painting->drawCircle(400, 330, 40, 30, 'black', 0, '',0);
+//background 2
+imagefilledrectangle($painting->getImageResource() , 0 , 420, $painting->getImageWidth() , $painting->getImageHeight() , $color['blue'] );
+
+//car brand || ugly
+/*$painting->createColorPalette('HEX','green', '#06ff00');
+$painting->createColorPalette('HEX','darkgreen', '#077c04');
+$painting->createColorPalette('HEX','blue', '#0024ff');*/
+
+/*$painting->drawCircle(400, 300, 300, 150, 'black', 10, 'green',1);
+$painting->drawCircle(400, 260, 200, 75, 'black', 10, 'green',1);
+$painting->drawCircle(400, 300, 100, 150, 'black', 10, 'green',1);*/
+
+$painting->renderImage();
+$painting->destroy();
 
 ?>
